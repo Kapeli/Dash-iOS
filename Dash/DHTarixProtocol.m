90b1875f3728f5ca4485693ec8209cf8342cecfe
Kapeli <support@kapeli.com>
2017-12-24 16:59:46 +0200

    Fixed access to UI on background thread. Not sure if it caused any issues, but the debugger complained about it

--- Dash/DHTarixProtocol.m
+++ Dash/DHTarixProtocol.m
@@ -81,7 +81,10 @@
         DHWebViewController *webController = [DHWebViewController sharedWebViewController];
         NSString *cssString = [NSString stringWithFormat:@"<head><style>%@</style>", [DHCSS currentCSSStringWithTextModifier]];
         
-        NSString *viewportString = [NSString stringWithFormat:@"<meta id='dash_viewport' name='viewport' content='%@'/></head>", [DHWebView viewportContent:webController.webView.frame]];
+        __block NSString *viewportString = @"";
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            viewportString = [NSString stringWithFormat:@"<meta id='dash_viewport' name='viewport' content='%@'/></head>", [DHWebView viewportContent:webController.webView.frame]];
+        });
         NSMutableString *content = [[NSMutableString alloc] initWithData:data encoding:NSUTF8StringEncoding];
         if(content)
         {
