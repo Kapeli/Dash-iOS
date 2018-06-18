0acc05c01aadb427a51579c2b7a048a32e825bdf
Kapeli <support@kapeli.com>
2018-06-18 19:46:27 +0300

    Add Xcode 10 support

--- Pods/Headers/Public/KissXML/KissXML.h
+++ Pods/Headers/Public/KissXML/KissXML.h
@@ -1,12 +1,1 @@
-@import Foundation;
-
-#import "DDXMLElementAdditions.h"
-#import "NSString+DDXML.h"
-#import "DDXML.h"
-#import "DDXMLDocument.h"
-#import "DDXMLElement.h"
-#import "DDXMLNode.h"
-
-FOUNDATION_EXPORT double KissXMLVersionNumber;
-FOUNDATION_EXPORT const unsigned char KissXMLVersionString[];
-
+../../../../Modified Pods/KissXML-5.1.2/KissXML/KissXML.h
